b7d2f817d697e17621e8a2bb794920888078f958
cookie <mail@vks.info>
2024-06-23 13:23:33 +0200

    $*

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializer import TodoSerializer
+from .models import Todo
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by('-created_at')
+    serializer_class = TodoSerializer
+    
---
+++ up.bat
@@ -0,0 +1,7 @@
+git add .
+
+git commit -m "$*"
+
+git push
+
+chrome "https://www.pythonanywhere.com/user/cookiecrack3r/consoles/34341813/"
